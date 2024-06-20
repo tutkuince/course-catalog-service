00f1c3a8039a11068ef4578bf4cb309f6d832eb8
Tutku Ince <tutku.ince@outlook.com>
2024-06-20 14:09:30 +0300

    set up logging in Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("io.github.microutils:kotlin-logging:3.0.5")
     runtimeOnly("com.h2database:h2")
     // runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
