5d7911e3b223b3475405e696b765fc55e09ff6ad
Tutku Ince <tutku.ince@outlook.com>
2024-06-20 13:11:34 +0300

    initial commit

---
+++ src/test/kotlin/com/ince/coursecatalogservice/CourseCatalogServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ince.coursecatalogservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CourseCatalogServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
