d61414dcd9e217757caa512fdda49f264a39e5f6
Tutku Ince <tutku.ince@outlook.com>
2024-06-20 15:29:43 +0300

    Unit Test for Controller - Using the Mockk Mocking library

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,8 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webflux")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("io.mockk:mockk:1.13.11")
+    testImplementation("com.ninja-squad:springmockk:4.0.2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
--- src/test/unit/com/ince/coursecatalogservice/controller/GreetingControllerUnitTest.kt
+++ src/test/unit/com/ince/coursecatalogservice/controller/GreetingControllerUnitTest.kt
@@ -1,20 +1,28 @@
 package com.ince.coursecatalogservice.controller
 
+import com.ince.coursecatalogservice.service.GreetingService
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ActiveProfiles
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.test.web.reactive.server.WebTestClient
 import kotlin.test.assertEquals
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ActiveProfiles("test")
+@WebMvcTest(controllers = [GreetingController::class])
 @AutoConfigureWebTestClient
-class GreetingControllerIntTest {
+class GreetingControllerUnitTest {
 
+    /**
+     * Unit test is a kind of test which tests only the class and method of interest and mocks the next layer of the code
+     *
+     * */
     @Autowired
     lateinit var webTestClient: WebTestClient
+
+    @MockkBean
+    lateinit var greetingServiceMock: GreetingService
 
     /**
      * Integration test is a kind of test which actually test the application end to end
@@ -23,6 +31,9 @@ class GreetingControllerIntTest {
     @Test
     fun retrieveGreetingTest() {
         val name = "Tutku"
+
+        every { greetingServiceMock.retrieveGreeting(any()) } returns "$name, Hello from default profile"
+
         val result = webTestClient.get()
             .uri("/v1/greetings/{name}", name)
             .exchange()
@@ -32,4 +43,5 @@ class GreetingControllerIntTest {
 
         assertEquals("$name, Hello from default profile", result.responseBody)
     }
+
 }
