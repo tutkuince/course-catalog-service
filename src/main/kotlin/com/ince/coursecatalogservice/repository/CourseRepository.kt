7424494f3b74efd1651e67d4e19c0fabc03fff64
Tutku Ince <tutku.ince@outlook.com>
2024-06-21 22:14:48 +0300

    Retrieve Courses by Name using JPQ Query Creation Function

--- src/main/kotlin/com/ince/coursecatalogservice/repository/CourseRepository.kt
+++ src/main/kotlin/com/ince/coursecatalogservice/repository/CourseRepository.kt
@@ -4,4 +4,7 @@ import com.ince.coursecatalogservice.entity.Course
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface CourseRepository: JpaRepository<Course, Int> {
+
+    // SELECT * FROM Course WHERE NAME LIKE :name
+    fun findCourseByNameContaining(name: String): List<Course>
 }
---
+++ src/test/int/com/ince/coursecatalogservice/repository/CourseRepositoryIntTest.kt
@@ -0,0 +1,30 @@
+package com.ince.coursecatalogservice.repository
+
+import com.ince.coursecatalogservice.util.courseEntityList
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.test.context.ActiveProfiles
+import kotlin.test.assertEquals
+
+@DataJpaTest
+@ActiveProfiles("test")
+class CourseRepositoryIntTest {
+
+    @Autowired
+    lateinit var courseRepository: CourseRepository
+
+    @BeforeEach
+    fun setUp() {
+        courseRepository.deleteAll()
+        courseRepository.saveAll(courseEntityList())
+    }
+
+    @Test
+    fun findCourseByNameContaining() {
+        val courses = courseRepository.findCourseByNameContaining("Spring Boot")
+        println("Courses: $courses")
+        assertEquals(2, courses.size)
+    }
+}
